3c5312f6321c5b047ff239c6663a2f2e699204e4
PavanSaiNekkala <91945047+PavanSaiNekkala@users.noreply.github.com>
2026-05-31 18:25:58 +0530

    Update generate_metadata.py

--- analytics/generate_metadata.py
+++ analytics/generate_metadata.py
@@ -15,8 +15,8 @@ INPUT_FILE = "data/valid_stocks.xlsx"
 OUTPUT_FILE = "data/stock_metadata.csv"
 
 MAX_WORKERS = 5
-BATCH_SIZE = 50
-BATCH_DELAY = 1
+BATCH_SIZE = 25
+BATCH_DELAY = 2
 
 # =====================================================
 # LOAD STOCKS
